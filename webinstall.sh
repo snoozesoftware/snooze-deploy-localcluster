295ccf44801d557a633a49d77721073ff0ecaa6c
msimonin <matthieu.simonin@inria.fr>
2014-04-04 10:46:11 +0200

    fix path
    M

--- webinstall.sh
+++ webinstall.sh
@@ -3,6 +3,7 @@
 # track the current directory
 TOP_DIR=$(cd $(dirname "$0") && pwd)
 SCRIPT_DIR="$TOP_DIR/snooze-deploy-localcluster"
+REPO="https://github.com/snoozesoftware/snooze-deploy-localcluster"
 
 if [ -z $(which git) ]
 then
@@ -18,8 +19,9 @@ then
   exit 1
 fi
 
-git clone https://github.com/msimonin/snooze-deploy-localcluster
-cd $SCRIPT_DIR && git checkout develop
+http_proxy=$http_proxy\
+https_proxy=$https_proxy\
+git clone $REPO
 
 # launch the automatic installer
 $SCRIPT_DIR/install.sh 
